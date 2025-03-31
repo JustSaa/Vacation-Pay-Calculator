4400eaecb35af0b92502608df2e119ef01149b56
Salavat <Justdrumm@yandex.ru>
2025-03-31 22:52:12 +0300

    feat: save vacation calculation results in PostgreSQL with salary, result, days and mode

--- src/main/java/org/example/vacation/application/VacationCalculationService.java
+++ src/main/java/org/example/vacation/application/VacationCalculationService.java
@@ -2,6 +2,9 @@ package org.example.vacation.application;
 
 import org.example.vacation.domain.VacationCalculation;
 
+import org.example.vacation.infrastructure.persistence.Mode;
+import org.example.vacation.infrastructure.persistence.entity.VacationCalculationEntity;
+import org.example.vacation.infrastructure.persistence.repository.VacationCalculationRepository;
 import org.example.vacation.util.WorkingDaysCalculator;
 import org.springframework.stereotype.Service;
 
@@ -11,13 +14,34 @@ import java.time.temporal.ChronoUnit;
 
 @Service
 public class VacationCalculationService {
+    private final VacationCalculationRepository repository;
+
+    public VacationCalculationService(VacationCalculationRepository repository) {
+        this.repository = repository;
+    }
 
     public BigDecimal calculate(BigDecimal averageSalary, int vacationDays) {
-        return VacationCalculation.calculateVacationPay(averageSalary, vacationDays);
+        BigDecimal result = VacationCalculation.calculateVacationPay(averageSalary, vacationDays);
+        repository.save(new VacationCalculationEntity(
+                averageSalary,
+                result,
+                vacationDays,
+                Mode.DAYS
+        ));
+
+        return result;
     }
 
     public BigDecimal calculate(BigDecimal averageSalary, LocalDate start, LocalDate end) {
         int workingDays = WorkingDaysCalculator.countWorkingDays(start, end);
-        return VacationCalculation.calculateVacationPay(averageSalary, workingDays);
+        BigDecimal result = VacationCalculation.calculateVacationPay(averageSalary, workingDays);
+        repository.save(new VacationCalculationEntity(
+                averageSalary,
+                result,
+                workingDays,
+                Mode.DATES
+        ));
+
+        return result;
     }
 }
